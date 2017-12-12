2ade916521861f17edfba01e47278def93dd46eb
Sean MacKenzie <sean.andrew.mackenzie@gmail.com>
2017-12-12 09:27:18 -0700

    trying to get the server going with correct code

--- server/handlers.js
+++ server/handlers.js
@@ -1,3 +1,27 @@
+let defaultErrorHandler = (err, req, res, next) => {
+	let error;
+	console.log('Error Caught:')
+	console.log(err);
+	if (res.headersSent) {
+		return next(err);
+	}
+	let env = process.env.NODE_ENV;
+	if (env !== 'production') {
+		error = {
+			ok: false,
+			error: err.message,
+			stack: err
+		}
+	} else {
+		error = {
+			ok: false,
+			error: err.message,
+		}
+	}
+	res.status(400).send(error);
+}
+
+//Default Urls 
 var whitelist = ['http://localhost:8080', 'http://portal.boisecodeworks.com'];
 var corsOptions = {
 	origin: function (origin, callback) {
@@ -7,6 +31,9 @@ var corsOptions = {
 	credentials: true
 };
 
+
+
 module.exports = {
-    corsOptions
+	defaultErrorHandler,
+	corsOptions
 }
--- server/index.js
+++ server/index.js
@@ -1,30 +1,47 @@
 var express = require('express')
-var server = express()
+// var server = express()
 var bp = require('body-parser')
 var cors = require('cors')
+var defaultErrorHandler = require('./handlers').defaultErrorHandler
 var corsOptions = require('./handlers').corsOptions
 var DBConnect = require('./config/mlab-config')
 var sessions = require('./auth/sessions')
+var Auth = require('./auth/auth')
 var port = 3000
 
 //route variables
 var userRoutes = require('./auth/auth')
 
+let app = express()
+let server = require('http').createServer(app)
+
 
 //register Middlewear
 // server.use(express.static(__dirname + '/public'))
-server.use(bp.json())
-server.use(bp.urlencoded({extended: true}))
-server.use(sessions)
+app.use(sessions)
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/../public'))
+app.use('*', logger)
+app.use('*', cors(corsOptions))
+app.use('/', Auth)
 
 ///register routes
-server.use(userRoutes)
-server.use(Authenticate)
+app.use(userRoutes)
+app.use(Validate)
+app.use('/', defaultErrorHandler)
 
-function Authenticate(req, res, next) {
-    if(!req.session.uid) {
-        return res.status(401).send({ error: 'You must login to continue' })
+function Validate(req, res, next) {
+    // ONLY ALLOW GET METHOD IF NOT LOGGED IN 
+    console.log(req.session)
+    if (req.method !== 'GET' && !req.session.uid) {
+        return res.status(401).send({ error: 'Please Login or Register to continue' })
     }
+    return next()
+}
+
+function logger(req, res, next) {
+    console.log('INCOMING REQUEST', req.url)
     next()
 }
 
@@ -32,3 +49,5 @@ function Authenticate(req, res, next) {
 server.listen(port, function(){
     console.log('Trying to get it done on: ', port)
 })
+
+module.exports = server
