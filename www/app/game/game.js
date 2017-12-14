f2b6c31134518a7e0024c30fb6b2c8d7dfc44b1f
Tim Sharp <timothyaaron.sharp@gmail.com>
2017-12-13 21:05:49 -0700

    fixed win condition to no longer win after you have lost and all the enemies are gone

--- www/app/game/game.js
+++ www/app/game/game.js
@@ -4,14 +4,65 @@ var game = new Phaser.Game(1024, 768, Phaser.AUTO, 'game-area');
 var gameData = {
     level: {
         name: 'The fall of rivendale',
-        map: 'assets/maps/level1.json',
+        map: {
+            "height": 24,
+            "layers": [
+                {
+                    "data": [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 32, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 32, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 32, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 32, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 32, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 32, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 32, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 32, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 32, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 32, 4, 4, 4, 31, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 3, 3, 3, 3, 3, 3, 9, 9, 9, 9, 9, 9, 9, 9, 9, 32, 4, 4, 4, 31, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 15, 14, 14, 14, 16, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 2, 18, 18, 18, 18, 18, 18, 18, 18, 32, 4, 4, 4, 31, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 34, 34, 34, 34, 34, 34, 13, 34, 34, 34, 34, 34, 34, 34, 34, 34, 4, 4, 4, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 4, 4, 4, 4, 4, 4, 11, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 11, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 11, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 33, 33, 33, 33, 33, 33, 12, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 39, 2, 19, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 32, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 39, 2, 19, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 32, 4, 4, 4, 4, 4, 9, 9, 9, 9, 9, 2, 2, 19, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 32, 4, 4, 4, 4, 4, 2, 2, 2, 2, 2, 2, 2, 19, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 32, 4, 4, 4, 4, 4, 18, 18, 18, 18, 18, 18, 18, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 32, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 32, 4, 4, 4, 4, 4],
+                    "height": 24,
+                    "name": "Ground",
+                    "opacity": 1,
+                    "type": "tilelayer",
+                    "visible": true,
+                    "width": 32,
+                    "x": 0,
+                    "y": 0
+                }],
+            "nextobjectid": 1,
+            "orientation": "orthogonal",
+            "renderorder": "left-down",
+            "tiledversion": "1.0.3",
+            "tileheight": 32,
+            "tilesets": [
+                {
+                    "columns": 10,
+                    "firstgid": 1,
+                    "image": "tiles.bmp",
+                    "imageheight": 132,
+                    "imagewidth": 330,
+                    "margin": 0,
+                    "name": "tiles",
+                    "spacing": 1,
+                    "tilecount": 40,
+                    "tileheight": 32,
+                    "tileproperties":
+                        {
+                            "3":
+                                {
+                                    "canBuild": false
+                                }
+                        },
+                    "tilepropertytypes":
+                        {
+                            "3":
+                                {
+                                    "canBuild": "bool"
+                                }
+                        },
+                    "tilewidth": 32
+                }],
+            "tilewidth": 32,
+            "type": "map",
+            "version": 1,
+            "width": 32
+        },
         mapKey: 'level1',
         tilesetImage: 'assets/images/tiles.bmp',
         tilesetImageName: 'tiles',
         tilesetImageKey: 'tiles',
         tilemapLayer: 'Ground',
         buildableTileId: 3,
-        playerLevelHealth: 7,
+        playerLevelHealth: 3,
         enemies: [{ //will need to make sure the schema has all the properties that we are adding below, such as spawnTime and gameObject.
             type: 'star',
             health: 100,
@@ -70,6 +121,7 @@ var gameData = {
 }
 
 var map, layer, selectedTile, winLoseText, pauseText
+var gameOver = false
 var gameClock = 0;
 var currentTileProperties = ''
 var numOfTowers = 1
@@ -101,7 +153,7 @@ PhaserGame.prototype = {
         }
         game.load.image(gameData.level.towers[0].bullet, gameData.level.towers[0].bulletSprite)
         // game.load.image('bullet', 'assets/images/bullet.png')
-        game.load.tilemap(gameData.level.mapKey, gameData.level.map, null, Phaser.Tilemap.TILED_JSON);
+        game.load.tilemap(gameData.level.mapKey, null, gameData.level.map, Phaser.Tilemap.TILED_JSON);
         game.load.image(gameData.level.tilesetImageKey, gameData.level.tilesetImage);
 
         for (let i = 0; i < gameData.level.enemies.length; i++) {
@@ -113,7 +165,7 @@ PhaserGame.prototype = {
 
     //create - function that creates the game world and everything in it.
     create: function () {
-        this.game.scale.pageAlignHorizontally = true;this.game.scale.pageAlignVertically = true;this.game.scale.refresh();
+        this.game.scale.pageAlignHorizontally = true; this.game.scale.pageAlignVertically = true; this.game.scale.refresh();
 
         game.physics.startSystem(Phaser.Physics.ARCADE) //starts the physics system.
         //bmd code allows for the plot to draw the plot points if you don't have the map loaded. Useful when making paths and for debugging.
@@ -245,7 +297,7 @@ PhaserGame.prototype = {
         towers.forEach(function (tower) {
             tower.aquireTarget(tower)
         });
-        if (gameState.enemiesOutOfPlay == gameData.level.enemies.length) {
+        if (gameState.enemiesOutOfPlay == gameData.level.enemies.length && !gameOver) {
             winLoseText.text = "A winner is YOU!"
             winLoseText.visible = true;
             // console.log(winLoseText)
@@ -328,6 +380,8 @@ PhaserGame.prototype = {
         console.log(gameState.enemiesOutOfPlay)
         console.log(gameData.level.enemies.length)
         if (gameState.playerHealth <= 0) {
+            gameOver = true
+            console.log(gameOver)
             winLoseText.text = "Game Over";
             winLoseText.visible = true;
         }
